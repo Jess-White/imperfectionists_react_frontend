c562a16d52fb2a09531e8720da252a2b4e30bd3d
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2021-01-08 18:08:11 -0600

    add forms and checkboxes

--- src/Components/BlogPostNew.js
+++ src/Components/BlogPostNew.js
@@ -4,63 +4,109 @@ import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
-function Form() {
+export default function BlogPostNew() {
   const [title, setTitle] = useState("");
   const [text, setText] = useState("");
   const [author, setAuthor] = useState("");
   const [imageUrl, setImageUrl] = useState("");
-  // const [wordocunt, setWordcount] = useState("");
-  // const [finished, setFinished] = useState("");
-  // const [reviewed, setReviewed] = useState("");
-  // const [favorite, setFavorite] = useState("");
+  // const [wordcount, setWordcount] = useState("");
+  const [finished, setFinished] = useState("");
+  const [reviewed, setReviewed] = useState("");
+  const [favorite, setFavorite] = useState("");
   // const [like, setLike] = useState("");
-}
+  const [name, setName] = useState("");
+  
 
-return (
-  <Form>
-    <Form.Group>
-      <Form.Label>Title</Form.Label>
-      <Form.Control 
-        type="text"
-        name="title"
-        value={title}
-        onChange={e => setTitle(e.target.value)}
-        placeholder="title"
-        required
-      />
-    </Form.Group>
-    <Form.Group>
-      <Form.Label>Text</Form.Label>
-      <Form.Control 
-        type="text"
-        name="text"
-        value={text}
-        onChange={e => setText(e.target.value)}
-        placeholder="text"
-        required
-      />
-    </Form.Group>
-    <Form.Group>
-      <Form.Label>Author</Form.Label>
-      <Form.Control 
-        type="text"
-        name="author"
-        value={author}
-        onChange={e => setAuthor(e.target.value)}
-        placeholder="author"
-        required
-      />
-    </Form.Group>
-    <Form.Group>
-      <Form.Label>ImageUrl</Form.Label>
-      <Form.Control 
-        type="text"
-        name="imageUrl"
-        value={imageUrl}
-        onChange={e => setImageUrl(e.target.value)}
-        placeholder="Image URL"
-        required
-      />
-    </Form.Group>
-  </Form>
-)
+  const handleChange = (event) => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    setName(value);
+  }
+
+  return (
+    <Card>
+      <Card.Header>Add New Felix Valotton Content!
+      </Card.Header>
+      <Card.Body>
+        <Form>
+          <Form.Group>
+            <Form.Label>Title</Form.Label>
+            <Form.Control 
+              type="text"
+              name="title"
+              value={title}
+              onChange={e => setTitle(e.target.value)}
+              placeholder="title"
+              required
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Text</Form.Label>
+            <Form.Control 
+              type="text"
+              name="text"
+              value={text}
+              onChange={e => setText(e.target.value)}
+              placeholder="text"
+              required
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Author</Form.Label>
+            <Form.Control 
+              type="text"
+              name="author"
+              value={author}
+              onChange={e => setAuthor(e.target.value)}
+              placeholder="author"
+              required
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>ImageUrl</Form.Label>
+            <Form.Control 
+              type="text"
+              name="imageUrl"
+              value={imageUrl}
+              onChange={e => setImageUrl(e.target.value)}
+              placeholder="Image URL"
+              required
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Finished</Form.Label>
+            <Form.Check
+              type="checkbox"
+              name="finished"
+              value="finished"
+              checked={finished}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Reviewed</Form.Label>
+            <Form.Check
+              type="checkbox"
+              name="reviewed"
+              value="reviewed"
+              checked={reviewed}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Favorite</Form.Label>
+            <Form.Check
+              type="checkbox"
+              name="favorite"
+              value="favorite"
+              checked={favorite}
+              onChange={handleChange}
+            />
+          </Form.Group>
+        </Form>
+      </Card.Body>
+    </Card>
+  );
+}
