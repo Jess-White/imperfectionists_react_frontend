7ee75a490d9a89a0c8220a20e448519576333c83
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2021-01-15 18:14:30 -0600

    added Navigation component

---
+++ src/Components/Navigation.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Navigation() {
+    return (
+        <Navbar bg="light" expand="lg" sticky="top">
+          <ul>
+              <div>
+              <Nav className="mr-auto">
+                <Nav.Item style={{fontSize: "150%"}} className="active">
+                  <Nav.Link href="/blog_posts">Dashboard</Nav.Link>
+                </Nav.Item>
+                <Nav.Item style={{fontSize: "150%"}} className="active">
+                  <Nav.Link href="/blog_posts-new">Grants</Nav.Link>
+                </Nav.Item>
+                </Nav>
+                </div>
+          </ul>
+        </Navbar>
+    );
+}
+
+export default withRouter(Navigation);
