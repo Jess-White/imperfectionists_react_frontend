202c326e24aeaf0546bf8e4e090567be567df65e
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2021-02-01 17:27:21 -0600

    added frontend component functions and hooks for AI API

--- src/Services/ArtInstituteAPI.js
+++ src/Services/ArtInstituteAPI.js
@@ -12,3 +12,30 @@ export const getShowArtwork = (searchResultId) => {
     return axios (`https://api.artic.edu/api/v1/artworks/${searchResultId}`)
         .then(response => response.data)
 }
+
+import { getArtworks, searchArtworks, getShowArtwork } from './services/ArtInstituteAPI';
+
+getArtworks() 
+  .then(response => {
+    setArtInstituteResults(response)
+    }).catch(error => {
+    setError("Something went wrong.")
+    })
+
+searchArtworks(query)
+  .then(response => {
+    setArtInstituteSearchResults(response)
+  }).catch(error => {
+    setError("Something went wrong.")
+  })
+
+getShowArtwork(searchResultId)
+  .then(response => {
+    setArtInstituteShow(response)
+  }).catch(error => {
+    setError("Something went wrong.")
+  })
+
+const [artInstituteResults, setArtInstituteResults] = useState([]);
+const [artInstituteSearchResults, setArtInstituteSearchResults] = useState([]);
+const [artInstituteShow, setArtInstituteShow] = useState([]);
